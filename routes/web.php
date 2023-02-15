2b1fc1667a44e007f805bc429a4a8b3c4ab56b30
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-02-15 15:21:42 +0100

    site done

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,41 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/home', function () {
+    $pages = [
+        'home',
+        'second',
+        'third'
+    ];
+    $page_chosen = 'home';
+    return view('home', compact('pages', 'page_chosen'));
+})->name('home');
+
+
+Route::get('/second', function () {
+
+    $pages = [
+        'home',
+        'second',
+        'third'
+    ];
+    
+    $page_chosen = 'second';
+
+    return view('second', compact('pages', 'page_chosen'));
+})->name('second');
+
+Route::get('/third', function () {
+
+    $pages = [
+        'home',
+        'second',
+        'third'
+    ];
+    
+    $page_chosen = 'third';
+
+    return view('third', compact('pages', 'page_chosen'));
+})->name('third');
